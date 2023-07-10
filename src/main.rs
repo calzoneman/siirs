18fe3f229751d5059b268353ca10c1a6420840e5
Calvin Montgomery <calzoneman@gmail.com>
2023-07-09 18:46:27 -0700

    Add some achievement tracking using the sqlite export

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use sii::{
     parser::Parser,
 };
 
+mod achivements;
 mod prospective_cities;
 mod sii;
 mod sqlite;
@@ -18,48 +19,46 @@ fn main() -> Result<()> {
         bail!("Usage: {} summary <file>", args[0]);
     }
 
-    let fname = &args[2];
-    let enc_file = File::open(fname)?;
-    let decrypted = Decryptor::new(enc_file).decrypt()?;
-    let zread = zlib::Decoder::new(decrypted.as_slice());
-    let save = GameSave::new(zread)?;
-
     match args[1].as_str() {
-        "summary" => {
-            let summary = SaveSummary::from_game_save(&save)?;
-            println!("{:?}", summary);
-        }
         "to-sqlite" => {
-            let enc_file = File::open(fname)?;
+            let enc_file = File::open(&args[2])?;
             let decrypted = Decryptor::new(enc_file).decrypt()?;
             let zread = zlib::Decoder::new(decrypted.as_slice());
             let parser = Parser::new(zread)?;
-            sqlite::copy_to_sqlite(parser, args[3].as_str())?;
+            sqlite::copy_to_sqlite(parser, &args[3])?;
         }
-        "prospective-cities" => {
-            prospective_cities::find_profitable_cities(&save)?;
+        "check-achievements" => {
+            achivements::check_achivement_status(&args[2])?;
         }
-        "debug-blocks" => {
-            for (id, block) in save.iter_blocks() {
-                println!("{}\tid={:?}", block.struct_name, id);
+
+        _ => {
+            let fname = &args[2];
+            let enc_file = File::open(fname)?;
+            let decrypted = Decryptor::new(enc_file).decrypt()?;
+            let zread = zlib::Decoder::new(decrypted.as_slice());
+            let save = GameSave::new(zread)?;
+
+            match args[1].as_str() {
+                "summary" => {
+                    let summary = SaveSummary::from_game_save(&save)?;
+                    println!("{:?}", summary);
+                }
+                "prospective-cities" => {
+                    prospective_cities::find_profitable_cities(&save)?;
+                }
+                "debug-list-blocks" => {
+                    for (id, block) in save.iter_blocks() {
+                        println!("{}\tid={:?}", block.struct_name, id);
+                    }
+                }
+                "debug-dump-blocks-named" => {
+                    for (_, block) in save.iter_blocks_named(&args[3]) {
+                        println!("{:?}", block);
+                    }
+                }
+                _ => bail!("unknown command {}", args[1]),
             }
         }
-        "debug-job-offers" => {
-            for (_, block) in save.iter_blocks_named("job_offer_data") {
-                println!("{:?}", block);
-            }
-        }
-        "debug-companies" => {
-            for (_, block) in save.iter_blocks_named("company") {
-                println!("{:?}", block);
-            }
-        }
-        "debug-garages" => {
-            for (_, block) in save.iter_blocks_named("garage") {
-                println!("{:?}", block);
-            }
-        }
-        _ => bail!("unknown command {}", args[2]),
     }
 
     Ok(())
